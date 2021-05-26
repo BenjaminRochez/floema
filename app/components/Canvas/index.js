524b7b8a2e12709799e46523083ebdbc2b328b0a
Benjamin Rochez <rochez.benjamin@gmail.com>
2021-05-26 07:45:10 +0200

    :sparkles: Implementing ogl homepage

--- app/components/Canvas/index.js
+++ app/components/Canvas/index.js
@@ -1,11 +1,14 @@
-import { Renderer, Camera, Transform, Mesh, Box, Program } from "ogl";
+import { Renderer, Camera, Transform } from "ogl";
+
+import Home from './Home'
+
 
 export default class Canvas {
   constructor() {
     this.createRenderer();
     this.createScene();
     this.createCamera();
-    this.createCube();
+    this.createHome();
   }
 
   createRenderer() {
@@ -16,32 +19,11 @@ export default class Canvas {
     document.body.appendChild(this.gl.canvas);
   }
 
-  createCube() {
-    this.geometry = new Box(this.gl);
-    this.program = new Program(this.gl, {
-      vertex: `
-          attribute vec3 position;
-
-          uniform mat4 modelViewMatrix;
-          uniform mat4 projectionMatrix;
-
-          void main() {
-              gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-          }
-          `,
-      fragment: `
-          void main() {
-              gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
-          }
-      `,
-    });
-
-    this.mesh = new Mesh(this.gl, {
-      geometry: this.geometry,
-      program: this.program,
-    });
-
-    this.mesh.setParent(this.scene);
+  createHome() {
+    this.home = new Home({
+      gl: this.gl,
+      scene: this.scene
+    })
   }
 
   createScene() {
@@ -61,8 +43,6 @@ export default class Canvas {
   }
 
   update() {
-    this.mesh.rotation.x += 0.01
-    this.mesh.rotation.y += 0.01
     this.renderer.render({
       camera: this.camera,
       scene: this.scene,
